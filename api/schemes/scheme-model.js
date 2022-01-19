755788e277689901f4c23490d503716db850afc9
wb1110 <strengthcoachwb@gmail.com>
2022-01-19 15:52:23 -0600

    getId

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -26,7 +26,7 @@ async function find() { // EXERCISE A
     return rows
 }
 
-function findById(scheme_id) { // EXERCISE B
+async function findById(scheme_id) { // EXERCISE B
   /*
     1B- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`:
 
@@ -41,6 +41,8 @@ function findById(scheme_id) { // EXERCISE B
 
     2B- When you have a grasp on the query go ahead and build it in Knex
     making it parametric: instead of a literal `1` you should use `scheme_id`.
+
+
 
     3B- Test in Postman and see that the resulting data does not look like a scheme,
     but more like an array of steps each including scheme information:
@@ -92,6 +94,31 @@ function findById(scheme_id) { // EXERCISE B
         "steps": []
       }
   */
+      const rows = await db('schemes as sc')
+      .leftJoin('steps as st', 'sc.scheme_id', '=', 'st.scheme_id')
+      .select('sc.scheme_name', 'st.*')
+      .where('sc.scheme_id', scheme_id)
+      .orderBy('st.step_number', 'asc')
+
+      const result = {
+        scheme_id: rows[0].scheme_id,
+        scheme_name: rows[0].scheme_name,
+        steps: []
+      }
+
+      rows.forEach(row => {
+        if (row.step_id) {
+          result.steps.push({
+            "step_id": row.step_id,
+            "step_number": row.step_number,
+            "instructions": row.instructions
+          })
+        }
+      })
+    
+
+      return result
+        
 }
 
 function findSteps(scheme_id) { // EXERCISE C
