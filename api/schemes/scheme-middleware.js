e16c8e6538a69ef7f619a5979d73d8015cf0944d
wb1110 <strengthcoachwb@gmail.com>
2022-01-19 09:48:34 -0600

    middleware done

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -35,8 +35,24 @@ const checkSchemeId = async (req, res, next) => {
     "message": "invalid scheme_name"
   }
 */
-const validateScheme = (req, res, next) => {
-
+const validateScheme = async (req, res, next) => {
+  try {
+    const { scheme_name } = req.body
+    if(
+      !scheme_name || 
+      scheme_name === '' || 
+      typeof scheme_name !== 'string'
+      ) {
+      next({ 
+        message: 'invalid scheme_name',
+        status: 400
+      })
+    } else {
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
 /*
@@ -49,7 +65,31 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
-
+  try {
+    const { instructions, step_number } = req.body
+    if(
+      !instructions || 
+      instructions === '' || 
+      typeof instructions !== 'string'
+      ) {
+      next({ 
+        message: 'invalid step',
+        status: 400
+      })
+    } else if (
+      typeof step_number !== 'number' || 
+      step_number < 1
+      ) {
+      next({ 
+        message: 'invalid step',
+        status: 400
+      })
+    } else {
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
 module.exports = {
